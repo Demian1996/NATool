2002396c473abb12ca54fe56873533d206e8874a
wangdengzhi <1303465909@qq.com>
2020-08-11 19:21:58 +0800

    [feature][what]增加汉家江湖链接

--- packages/data-fetch/index.ts
+++ packages/data-fetch/index.ts
@@ -1,2 +1,6 @@
 import superagent from 'superagent';
 import cheerio from 'cheerio';
+
+superagent.get('https://wiki.biligame.com/jianghux/%E4%BE%A0%E5%AE%A2').then((res) => {
+  console.log(res);
+});
