91355764715d0ef2cd212409d0c1e082faf7ea33
王登芝 <1303465909@qq.com>
2020-08-12 23:04:48 +0800

    [feature][what]解析table

--- packages/data-fetch/index.ts
+++ packages/data-fetch/index.ts
@@ -2,5 +2,8 @@ import superagent from 'superagent';
 import cheerio from 'cheerio';
 
 superagent.get('https://wiki.biligame.com/jianghux/%E4%BE%A0%E5%AE%A2').then((res) => {
-  console.log(res);
+  const $ = cheerio.load(res.text);
+
+  const getTable = () => $('#CardSelectTr');
+  getTable().text();
 });
